c0ef07e9e7ad7bdca6c98f25b402f22afdeb8e3b
Enock <enock.mokua@student.moringaschool.com>
2022-12-19 19:26:48 +0300

    create user attributes

--- db/migrate/20221219162544_create_users.rb
+++ db/migrate/20221219162544_create_users.rb
@@ -1,6 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.string :email
+      t.string :username
+      t.integer :amount
+      t.integer :phone
 
       t.timestamps
     end
